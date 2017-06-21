42cef9f3759dd44a8ca75ce704df3d63e53f2d4e
Robert Row <studentofdata@gmail.com>
2017-06-21 13:48:02 -0700

    Beginning to play around with the idea of implementing a technology paper

---
+++ src/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'e0SKkFt8jcBBqqF8GH3T2XMFd'
+consumer_secret = 'jpssszS9kRygjTUlj9jcn75DmwpkxfCBlB5jiPkOqAQaezHZly'
+access_token_key = '713582353281495040-QcmwkV9v3DyBb1yn00zWNoGdJ1UNrEe'
+access_token_secret = 'oX0ShHTdwHHzCWqhQn56J1Uch4megHMVLNoWdIQiZHkbB'
